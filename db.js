c41af14e70c804581b56fcd779d5d034f9ff84cc
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2016-03-06 23:06:14 +0100

    Read database url from env

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 var knex = require('knex');
 
+var CONNECTION = process.env.DATABASE_URL || 'postgres://127.0.0.1/elite';
+
 var db = knex({
 	client: 'pg',
-	connection: {
-		host: '127.0.0.1',
-		database: 'elite'
-	}
+	connection: CONNECTION
 });
 
 module.exports = db;
